7c398afb35384709b9d9bb91893d98cd9858b421
meghakoshta <55126927+meghakoshta@users.noreply.github.com>
2026-04-08 16:29:08 +0530

    Disable response compression
    Added middleware to disable compression for all responses.

--- api/server.js
+++ api/server.js
@@ -3,7 +3,15 @@ const app = express();
 
 app.use(express.json({ limit: '15mb' }));
 
+// Disable compression for all responses
+app.use((req, res, next) => {
+  res.setHeader('Content-Encoding', 'identity');
+  res.setHeader('Cache-Control', 'no-transform');
+  next();
+});
+
 const echo = require('./api/echo');
 app.all('/api/echo', echo);
 
 app.listen(process.env.PORT || 3000);
+
